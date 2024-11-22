ad69754dcb963f93d41f6cf5d5ae2f92b01526f4
Prosantaroy1 <royprosanta172@gmail.com>
2024-11-22 03:19:54 -0800

    add to contact page or data collect

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -2,20 +2,21 @@ import { useEffect, useState } from "react";
 import CoverBannerPage from "../../components/common/CoverBannerPage";
 import BlogCard from "../../components/common/BlogCard";
 import Sectionheading from "../../components/common/Sectionheading";
+import Loading from "../../components/common/loading";
 
 const BlogPage = () => {
 
     // create to state
-    const [blogs, setBlogs]= useState([]);
+    const [blogs, setBlogs] = useState([]);
 
     // add effect 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('/public/BlogData.json')
-        .then(res=> res.json())
-        .then(data=>{
-            setBlogs(data)
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data)
+            })
+    }, [])
 
 
     return (
@@ -27,15 +28,18 @@ const BlogPage = () => {
             <div className="container mt-12">
                 <div className="py-12">
                     <Sectionheading
-                     headingTitle='Letest Blog'
-                     headingsubtitle='Lorem ipsum dolor sit amet.'
+                        headingTitle='Letest Blog'
+                        headingsubtitle='Lorem ipsum dolor sit amet.'
                     />
                 </div>
-                <div className="grid grid-cols-1 items-center   gap-5 md:grid-cols-2 lg:grid-cols-3">
-                    {
-                        blogs.map((blog,idx)=><BlogCard key={idx} blog={blog}></BlogCard>)
-                    }
-                </div>
+                {
+                    blogs.length > 0 ?
+                        <div className="grid grid-cols-1 items-center   gap-5 md:grid-cols-2 lg:grid-cols-3">
+                            {
+                                blogs.map((blog, idx) => <BlogCard key={idx} blog={blog}></BlogCard>)
+                            }
+                        </div> : <Loading/>
+                }
             </div>
         </div>
     );
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,70 @@
+import { useForm } from "react-hook-form";
 import CoverBannerPage from "../../components/common/CoverBannerPage";
+import { useState } from "react";
 
 const Contact = () => {
+
+    const [collects, setCollects]= useState([]);
+
+    //data form handle
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    //data
+    const onSubmit = (data) => {    
+        const newData=[...collects, data];
+        setCollects(newData)
+
+    }
+    console.log(collects)
+
     return (
         <div>
             {/* cover banner */}
-             <CoverBannerPage
-                 img='https://utfs.io/f/oI7Ou0bdQ6rjYDCfTpVgoqDshSiVxC6IWeyk5aTJ1jQGwuRL'
-                 titleName='Contact'
-                 />
+            <CoverBannerPage
+                img='https://utfs.io/f/oI7Ou0bdQ6rjYDCfTpVgoqDshSiVxC6IWeyk5aTJ1jQGwuRL'
+                titleName='Contact'
+            />
+            <div className="container mt-16">
+                {/* contact form */}
+                <div className="font-Poppin">
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                        <div className="flex flex-col gap-y-3">
+                            <label className="text-xl font-semibold">Name :</label>
+                            <input placeholder="Enter your name?"
+                                className="w-1/2 px-2 py-3 border-2 border-black rounded-md"
+                                {...register("name",{ required: true })}
+                            />
+                            {errors.name && <span className="text-red-500">This field is required</span>}   
+                        </div>
+                        <div className="flex flex-col gap-y-3">
+                            <label className="text-xl font-semibold">Email :</label>
+                            <input placeholder="Enter your email?"
+                                className="w-1/2 px-2 py-3 border-2 border-black rounded-md"
+                                {...register("email",{ required: true }) }
+                            />
+                          {errors.email && <span className="text-red-500">This field is required</span>}                          
+                        </div>
+                        {/* submit btn */}
+                        <input type="submit" className="bg-green-500 cursor-pointer px-12 font-medium py-3 text-lg rounded-md text-white" />
+                    </form>
+                </div>
+                {/* data collection show */}
+                <div className="font-Poppins mt-9">
+                    <h3 className="text-2xl font-medium leading-7">Collection Data User</h3>
+                    <div className="pt-5 space-y-5">
+                       {collects.map((item,idx)=><div key={idx}>
+                                <div className="flex text-black items-center gap-x-8 ">
+                                    <p>{idx+1}</p>
+                                    <h3>{item.name}</h3>
+                                    <p>{item.email}</p>
+                                </div>
+                       </div>)}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
